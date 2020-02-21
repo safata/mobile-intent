dc5369c63f54aa4638aa027cc0e459cbef978883
safata <annisasafataa@gmail.com>
2020-02-21 08:40:19 +0700

    Parcelable

--- app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -1,15 +1,34 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ExplicitIntentActivity extends AppCompatActivity {
+public class ExplicitIntentActivity<EditText> extends AppCompatActivity {
 
+
+    private android.widget.EditText nameInput;
+    private TextView outputText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        nameInput= findViewById(R.id.input_name);
+        outputText= findViewById(R.id.text_output);
     }
+
+    public void handleExplicitIntent(View view) {
+    }
+
+    public void handleEditText (View view){
+
+        String nama = nameInput.getText().toString();
+        outputText.setText(nama);
+    }
+
+
 
 }
---
+++ app/src/main/java/id/ac/polinema/intent/model/User.java
@@ -0,0 +1,40 @@
+package id.ac.polinema.intent.model;
+
+public class User {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String name;
+    private int age;
+
+    public User(String username, String name, int age) {
+        this.username = username;
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
