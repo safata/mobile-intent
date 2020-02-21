a5da3a7b1937c4ed267da82d227c07617c36699b
safata <annisasafataa@gmail.com>
2020-02-21 08:50:08 +0700

    Parcelable

--- app/src/main/java/id/ac/polinema/intent/model/User.java
+++ app/src/main/java/id/ac/polinema/intent/model/User.java
@@ -1,6 +1,9 @@
 package id.ac.polinema.intent.model;
 
-public class User {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
     private String username;
 
     public String getUsername() {
@@ -37,4 +40,33 @@ public class User {
     }
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.username);
+        dest.writeString(this.name);
+        dest.writeInt(this.age);
+    }
+
+    protected User(Parcel in) {
+        this.username = in.readString();
+        this.name = in.readString();
+        this.age = in.readInt();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel source) {
+            return new User(source);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 }
